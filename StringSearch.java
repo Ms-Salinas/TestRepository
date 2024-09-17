d62753803a0388ca9f7c16f0b44225cf8b68fdd9
MrsChapa <123482950+MrsChapa@users.noreply.github.com>
2024-09-17 10:57:11 -0500

    Update StringSearch.java
    updated

--- StringSearch.java
+++ StringSearch.java
@@ -8,7 +8,7 @@ public class StringSearch
 {
 	/*
 	 *method countWords will count the occurrences of word in sent
-	 *there may many occurrences of word or none at all
+	 *there may many occurrences of word or none at all these are my changes!!!!!!!!!!
 	 */
 	public static int countWords(String sent, String word)
 	{
